4dbe8c5a866337c7b2a92aefa1619e7da6f5b122
Meg Sandman <margaret.sandman@gmail.com>
2014-11-28 12:18:59 -0800

    add angular to asset pipeline

--- chefboard/app/assets/javascripts/application.js
+++ chefboard/app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require ../../../public/javascripts/angular.min
+//= require ../../../public/javascripts/angular.min.js.map
+//= require ../../../public/javascripts/angular-route.min.js.map
+//= require ../../../public/javascripts/angular.min.js
 //= require_tree .
