c8165f5e13f6751575f2520ab57619c3ee92e961
cgdibble <cgdibble@gmail.com>
2014-11-28 18:44:11 -0800

    removed user tests since there are no routes to test

--- chefboard/spec/controllers/users_controller_spec.rb
+++ chefboard/spec/controllers/users_controller_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-  let!(:user_params){{username: "charlie", email: "charlie@charlie.com", password: "charlie"}}
 
-  context "#index" do
-    it "redirects you to your recipes" do
-      get :index, user_id: 1, user: user_params
-      # expect(response).to be_redirect
-    end
-  end
 end
