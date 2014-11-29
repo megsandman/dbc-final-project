f808a7117450b73379c46a4b38210795fde91295
Meg Sandman <margaret.sandman@gmail.com>
2014-11-28 20:53:16 -0800

    add images

--- cb-client/app/app.js
+++ cb-client/app/app.js
@@ -20,35 +20,35 @@ angular.module('Chefboard').controller('BoardController', function ($scope) {
     {
       title: "Chocolate Chip Cookies",
       source_url: "http://www.nytimes.com/2008/07/09/dining/091crex.html?_r=0",
-      img_url: "../app/assets/images/chocolate-chip-cookie.jpg",
+      img_url: "../assets/images/chocolate-chip-cookie.jpg",
       category_id: 1,
       user_id: 1
     },
     {
       title: "Pizza Dough",
       source_url: "http://www.foodnetwork.com/recipes/bobby-flay/pizza-dough-recipe.html",
-      img_url: "../app/assets/images/pizza-dough.jpeg",
+      img_url: "../assets/images/pizza-dough.jpeg",
       category_id: 2,
       user_id: 1
     },
     {
       title: "Kale and Citrus Salad",
       source_url: "http://www.saveur.com/article/Recipes/Winter-Kale-and-Citrus-Salad",
-      img_url: "../app/assets/images/kale-citrus-salad.jpg",
+      img_url: "../assets/images/kale-citrus-salad.jpg",
       category_id: 3,
       user_id: 1
     },
     {
       title: "Oatmeal",
       source_url: "http://www.foodnetwork.com/recipes/ina-garten/sunday-morning-oatmeal-recipe.html",
-      img_url: "../app/assets/images/oatmeal.jpeg",
+      img_url: "../assets/images/oatmeal.jpeg",
       category_id: 4,
       user_id: 1
     },
     {
       title: "Sangria",
       source_url: "http://www.foodnetwork.com/recipes/emeril-lagasse/sangria-recipe4.html",
-      img_url: "../app/assets/images/sangria.jpg",
+      img_url: "../assets/images/sangria.jpg",
       category_id: 5,
       user_id: 1
     }
