d36c063c4a5d2579d57caabd75ad38d3f420a2dd
Meg Sandman <margaret.sandman@gmail.com>
2014-11-29 12:23:56 -0800

    get route working

--- chefboard/app/assets/javascripts/application.js
+++ chefboard/app/assets/javascripts/application.js
@@ -15,65 +15,69 @@
 
 angular.module('Chefboard', []);
 
-angular.module('Chefboard').controller('BoardController', function ($scope) {
-  $scope.recipes =[
-    {
-      title: "Chocolate Chip Cookies",
-      source_url: "http://www.nytimes.com/2008/07/09/dining/091crex.html?_r=0",
-      img_url: "assets/chocolate-chip-cookie.jpg",
-      category_id: 1,
-      user_id: 1
-    },
-    {
-      title: "Pizza Dough",
-      source_url: "http://www.foodnetwork.com/recipes/bobby-flay/pizza-dough-recipe.html",
-      img_url: "assets/pizza-dough.jpeg",
-      category_id: 2,
-      user_id: 1
-    },
-    {
-      title: "Kale and Citrus Salad",
-      source_url: "http://www.saveur.com/article/Recipes/Winter-Kale-and-Citrus-Salad",
-      img_url: "assets/kale-citrus-salad.jpg",
-      category_id: 3,
-      user_id: 1
-    },
-    {
-      title: "Cranberry pie with thick pecan crumble",
-      source_url: "http://smittenkitchen.com/blog/2014/11/cranberry-pie-with-thick-pecan-crumble/",
-      img_url: "assets/cranberry-pie-with-thick-pecan-crumble.jpg",
-      category_id: 4,
-      user_id: 1
-    },
-    {
-      title: "Sangria",
-      source_url: "http://www.foodnetwork.com/recipes/emeril-lagasse/sangria-recipe4.html",
-      img_url: "assets/sangria.jpg",
-      category_id: 5,
-      user_id: 1
-    },
-    {
-      title: "Crispy Sweet Potato Roast",
-      source_url: "http://smittenkitchen.com/blog/2014/11/crispy-sweet-potato-roast/",
-      img_url: "assets/crispy-sweet-potato-roast.jpg",
-      category_id: 1,
-      user_id: 1
-    },
-    {
-      title: "Butternut Squash, Ricotta, and Sage Crostini",
-      source_url: "http://www.epicurious.com/recipes/food/views/Butternut-Squash-Ricotta-and-Sage-Crostini-367711",
-      img_url: "assets/sage-crostini.jpg",
-      category_id: 1,
-      user_id: 1
-    },
-    {
-      title: "Chicken Marsala",
-      img_url: "assets/chicken-marsala.jpg",
-      source_url: "http://www.epicurious.com/recipes/food/views/Chicken-Marsala-232152",
-      category_id: 2,
-      user_id: 1
-    }
-  ];
+angular.module('Chefboard').controller('BoardController', function ($scope, $http) {
+  $http.get('/users/1/recipes').success(function(data) {
+    $scope.recipes = data;
+    console.log(data);
+  });
+  // [
+  //   {
+  //     title: "Chocolate Chip Cookies",
+  //     source_url: "http://www.nytimes.com/2008/07/09/dining/091crex.html?_r=0",
+  //     img_url: "assets/chocolate-chip-cookie.jpg",
+  //     category_id: 1,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Pizza Dough",
+  //     source_url: "http://www.foodnetwork.com/recipes/bobby-flay/pizza-dough-recipe.html",
+  //     img_url: "assets/pizza-dough.jpeg",
+  //     category_id: 2,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Kale and Citrus Salad",
+  //     source_url: "http://www.saveur.com/article/Recipes/Winter-Kale-and-Citrus-Salad",
+  //     img_url: "assets/kale-citrus-salad.jpg",
+  //     category_id: 3,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Cranberry pie with thick pecan crumble",
+  //     source_url: "http://smittenkitchen.com/blog/2014/11/cranberry-pie-with-thick-pecan-crumble/",
+  //     img_url: "assets/cranberry-pie-with-thick-pecan-crumble.jpg",
+  //     category_id: 4,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Sangria",
+  //     source_url: "http://www.foodnetwork.com/recipes/emeril-lagasse/sangria-recipe4.html",
+  //     img_url: "assets/sangria.jpg",
+  //     category_id: 5,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Crispy Sweet Potato Roast",
+  //     source_url: "http://smittenkitchen.com/blog/2014/11/crispy-sweet-potato-roast/",
+  //     img_url: "assets/crispy-sweet-potato-roast.jpg",
+  //     category_id: 1,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Butternut Squash, Ricotta, and Sage Crostini",
+  //     source_url: "http://www.epicurious.com/recipes/food/views/Butternut-Squash-Ricotta-and-Sage-Crostini-367711",
+  //     img_url: "assets/sage-crostini.jpg",
+  //     category_id: 1,
+  //     user_id: 1
+  //   },
+  //   {
+  //     title: "Chicken Marsala",
+  //     img_url: "assets/chicken-marsala.jpg",
+  //     source_url: "http://www.epicurious.com/recipes/food/views/Chicken-Marsala-232152",
+  //     category_id: 2,
+  //     user_id: 1
+  //   }
+  // ];
 
   $scope.addRecipe = function() {
     $scope.recipes.unshift({title: $scope.recipeTitle, source_url: $scope.recipeLink, img_url: $scope.imageLink, category_id: $scope.category, tags: $scope.recipeTags})
--- chefboard/app/controllers/recipes_controller.rb
+++ chefboard/app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   def index
     # user = User.find(params[:user_id])
     # @recipes = Recipe.where(user_id: user.id)
+    @recipes = Recipe.where(user_id: 1)
     render json: @recipes.to_json
   end
 
