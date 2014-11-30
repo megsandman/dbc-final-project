4547033e2642b2eee2c2d7caf061ac99e7ddd55a
cgdibble <cgdibble@gmail.com>
2014-11-30 09:16:59 -0800

    added chefboard to extension manifest, headers to recipes-controller

--- app/assets/extension/popup.js
+++ app/assets/extension/popup.js
@@ -1,5 +1,43 @@
 console.log("page ready man")
 
+
+//select images on the page
+$(document).ready(function() {
+  var activeTab;
+  chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+     // since only one tab should be active and in the current window at once
+     // the return variable should only have one entry
+   activeTab = arrayOfTabs[0];
+     // var activeTabId = arrayOfTabs[0].id; // or do whatever you need
+    findImages(activeTab);
+  });
+
+  function findImages(browserPage) {
+    console.log(browserPage);
+  }
+
+  function getPageDetails(callback) {
+    chrome.tabs.executeScript(retrieveBrowserDetails);
+    chrome.runtime.onMessage.addListener(function(message)  {
+        callback(message);
+    });
+  };
+
+  function retrieveBrowserDetails() {
+    chrome.runtime.sendMessage({
+    'title': document.title,
+    'source_url': window.location.href,
+    // 'comment': window.getSelection().toString()
+    });
+  };
+
+});
+
+
+
+
+
+
 $('#new-recipe').on('submit', function(event) {
   event.preventDefault();
   var recipeData = {
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :set_headers
 
   skip_before_filter  :verify_authenticity_token
 
@@ -51,5 +52,12 @@ class RecipesController < ApplicationController
   def recipe_params
     #don't require user_id because that will be done automatically within the actions based on the params[:user_id]
     params.require(:recipe).permit(:title, :source_url, :img_url, :body, :category_id)
+  end
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
