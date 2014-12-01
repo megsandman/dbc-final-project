a1c461d1c371ed826acde621d5b726663a4c31f7
Meg Sandman <margaret.sandman@gmail.com>
2014-12-01 12:11:13 -0800

    adjust data type of facebook id in users table

--- db/migrate/20141128193928_create_users.rb
+++ db/migrate/20141128193928_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.bigint :facebook_id
+      t.integer :facebook_id
       t.string :email
       t.string :access_key
 
