d7cbaec6a93dd8f9bcea196e00b40007968b951d
Andrea Braunstein <acbraunstein@gmail.com>
2014-11-30 17:51:17 -0800

    dialog functionality works but need to fix css positioning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,12 +6,21 @@ angular.module('Chefboard').controller('BoardController', function ($scope, $htt
   });
 
 
-  $scope.clickToOpen = function (recipeImgUrl) {
+  $scope.clickToOpen = function (recipeImgUrl, recipeTitle, recipeId) {
     ngDialog.open({
-      template: '<p><img src=' + recipeImgUrl + '></p>',
+      // template: '<div class="lightbox">' +
+      //               '<a href="">' +
+      //                 '<img class="lb-image" src=' + recipeImgUrl + '>' +
+      //               '</a>' + recipeTitle +
+      //           '</div>',
+       template: '<div class="lightbox">' +
+                    '<img class="lb-image" src=' + recipeImgUrl + '>' +
+                    '<a><p>' + recipeTitle + '</p></a>' +
+                '</div>',
+     // template: "<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>",
       plain: true
     });
-    console.log(recipeImgUrl);
+    console.log(recipeImgUrl, recipeTitle);
   }
 
 
