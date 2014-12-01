fd0ae2e19e4e58d8467ae25e36f11e014bbf67d3
Meg Sandman <margaret.sandman@gmail.com>
2014-12-01 11:37:25 -0800

    commented out checkLoginState function

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -22,11 +22,13 @@
     }
   }
   // This function is called when someone finishes with the Login Button.  See the onlogin handler attached to it in the sample code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+
+  // MEG - commented this out and still works, not sure what this piece does
+  // function checkLoginState() {
+  //   FB.getLoginStatus(function(response) {
+  //     statusChangeCallback(response);
+  //   });
+  // }
 
   window.fbAsyncInit = function() {
   FB.init({
