50f5842a138fb05bb2531387d07f9a0078665f7d
Andrea Braunstein <acbraunstein@gmail.com>
2014-11-30 19:43:03 -0800

    PROGRESSgit add .! Displays properly, must add link + more styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@ angular.module('Chefboard').controller('BoardController', function ($scope, $htt
       //           '</div>',
        template: '<div class="lightbox">' +
                     '<img class="lb-image" src=' + recipeImgUrl + '>' +
-                    '<a><p>' + recipeTitle + '</p></a>' +
+                    '<a href=' + recipeImgUrl + '><p>' + recipeTitle + '</p></a>' +
                 '</div>',
      // template: "<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>",
       plain: true
