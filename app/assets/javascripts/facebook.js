9fe6ac6c2ee3ba8a23e2c5425564c58a17c11b51
Meg Sandman <margaret.sandman@gmail.com>
2014-12-01 16:56:18 -0800

    break out html into login and dashboard, add fbook sdk to index

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,4 +1,3 @@
- // </script>
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
@@ -54,14 +53,7 @@
 
   };
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made
