90ef98bc3d7d95363c03a4f03ad270b923f55d9b
cgdibble <cgdibble@gmail.com>
2014-12-02 08:24:52 -0800

    formatted picture display/layout

--- app/assets/extension/popup.js
+++ app/assets/extension/popup.js
@@ -1,94 +1,49 @@
 $(document).ready( function() {
-  var form =
+
+  function getForm() {
+    var form =
       '<label for="title">Title:</label><input type="text" class="recipe-title thick-txt-bx" name="title" placeholder="Title"><br><br><label for="source_url">Recipe Url:</label><input type="text" class="recipe-source-url thick-txt-bx" name="source_url" placeholder="Recipe Url"><br><br><label for="img_url">Image Url:</label><input type="text" class="recipe-img-url thick-txt-bx" name="img_url"><br><br><label for="tags">Tags:</label><input type="text" class="recipe-tags thick-txt-bx" name="tags" placeholder="i.e. healthy, fast, easy"><br><br><select class="categories thick-txt-bx"><option value="" disabled selected>Select Recipe Category</option><option value="Appetizers">Appetizers</option><option value="Beverages">Beverages</option><option value="Breakfast">Breakfast</option><option value="Entrees">Entrees</option><option value="Salads">Salads</option><option value="Sides">Sides</option></select><br><br> <input type="submit" class="submit-recipe thick-txt-bx"  value="Create Pin">';
-
-  var loggedOut = '<p>You are not logged in</p><a href="http://chefboard.herokuapp.com/"><button class="chefboard-btn">chefboard.</button></a>'
-  var testNum = 1
-
-function renderExtension() {
-  if (testNum == 1) {
-    $('#new-recipe').append(form);
-  } else {
-    $('.logged-out').append(loggedOut);
-    $('#new-recipe').toggle();
+    return form;
   }
-}
 
-  // renderExtension();
+  function getLoggedOut() {
+    var loggedOut = '<p>You are not logged in</p><a href="http://chefboard.herokuapp.com/"><button class="chefboard-btn">chefboard.</button></a>'
+    return loggedOut;
+  }
 
-////////////////////////////////
-/////// BUILD NEW RECIPE ///////
-////////////////////////////////
-
-$('#new-recipe').on('submit', function(event) {
-  event.preventDefault();
-
-  var recipeData = {
-    recipe: {
-      title: $('.recipe-title').val(),
-      source_url: $('.recipe-source-url').val(),
-      img_url: $('.recipe-img-url').val(),
-    },
-    tags: $('.recipe-tags').val(),
-    category: $('.categories').val()
-  };
-
-  $.ajax({
-    type: 'POST',
-    // url: 'http://chefboard.herokuapp.com/users/1/recipes',
-    url: 'http://localhost:3000/users/1/recipes',
-    data: recipeData,
-    crossDomain: true,
-    success: function( response ) {
-      console.log(response);
-    },
-    error: function( error ) {
-      console.log(error);
+  function renderExtension() {
+    var testNum = 1 //test value for checking the toggling between form/loggedout prompt
+    if (testNum == 1) {
+      $('#new-recipe').append(getForm);
+    } else {
+      $('.logged-out').append(getLoggedOut);
+      $('#new-recipe').toggle();
     }
-  })
-  .done( function(serverData) {
-      console.log("THIS WAS A SUCCESS");
-      // close extension after ajax sent...hopefully
-      // window.close();
-  })
-  .fail( function(serverData) {
-      console.log("FAILURE");
-      // window.close();
-
-  });
-});
-///////  END NEW RECIPE  ///////
+  }
 
 /////////////     EXTRACT TITLE/URL FROM PAGE    /////////////
+  chrome.tabs.query({active: true, currentWindow:true}, function(array) {
+    var currentPage = array[0];
+    var currentUrl = currentPage.url;
+    var currentTitle = currentPage.title;
+  });
 
-chrome.tabs.query({active: true, currentWindow:true}, function(array) {
-  var currentPage = array[0];
-  var currentUrl = currentPage.url;
-  var currentTitle = currentPage.title;
-    images = scrapeImage(currentPage);
-
-    console.log(images);
-});
-
-/////    IMAGE SELECT    //////
-// go to currentPage.url and scrape that bad boy
-function scrapeImage(currentPage) {
+  /////    IMAGE SELECT    //////
+  // go to currentPage.url and scrape that bad boy of it's images!
+  function scrapeImages(currentPage) {
     $.ajax({
       url: currentPage.url,
       type: 'get',
-      dataType: "", //JSONP?
+      dataType: "",
       success: function(data) {
-        var images = [];
-        $magic = $('<form>' + data + '</form>')
+        $pageData = $('<form>' + data + '</form>') //has to be in the form for some reason
 
         //find images in the document
-        $.each($magic.find('img[src]'), function(index, item) {
+        $.each($pageData.find('img[src]'), function(index, item) {
           image_src = $(item).attr('src')
           $('<img src=' + image_src + '>').appendTo('.scraped-images');
-          images.push(image_src);
         })
         addImageListeners(currentPage);
-        return images
       },
       error: function(status) {
         console.log("it didn't work");
@@ -101,9 +56,50 @@ function scrapeImage(currentPage) {
       $('#new-recipe').append(form);
       $('.scraped-images').toggle();
       var src = $(this).attr('src');
-      $('.recipe-img-url').val(src);
-      $('.recipe-source-url').val(currentPage.url);
-      $('.recipe-title').val(currentPage.title)
+
+      populateFields(currentPage, src);
     });
   }
-});/// END ON READY
+
+  function populateFields(currentPage, src) {
+    $('.recipe-source-url').val(currentPage.url);
+    $('.recipe-title').val(currentPage.title)
+    $('.recipe-img-url').val(src);
+  }
+
+  ////////////////////////////////
+  /////// BUILD NEW RECIPE ///////
+  ////////////////////////////////
+
+  function getFormData() {
+    var recipeData = {
+      recipe: {
+        title: $('.recipe-title').val(),
+        source_url: $('.recipe-source-url').val(),
+        img_url: $('.recipe-img-url').val(),
+      },
+      tags: $('.recipe-tags').val(),
+      category: $('.categories').val()
+    };
+    return recipeData;
+  }
+
+  $('#new-recipe').on('submit', function(event) {
+    event.preventDefault();
+    var recipeData = getFormData;
+
+    $.ajax({
+      type: 'POST',
+      // url: 'http://chefboard.herokuapp.com/users/1/recipes',
+      url: 'http://localhost:3000/users/1/recipes',
+      data: recipeData,
+      crossDomain: true,
+      success: function( response ) {
+        console.log(response);
+      },
+      error: function( error ) {
+        console.log(error);
+      }
+    });
+  }); ///////  END NEW RECIPE  ///////
+});/// END DOCUMENT ON READY
