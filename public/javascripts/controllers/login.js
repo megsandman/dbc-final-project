33d0473ef637083e8c14b5c13bf78814c9e32966
Meg Sandman <margaret.sandman@gmail.com>
2014-12-03 11:39:00 -0800

    deleting a bunch of comments

--- public/javascripts/controllers/login.js
+++ public/javascripts/controllers/login.js
@@ -1,14 +1,9 @@
 app.controller("LoginController", ["$scope", "$http", "$location", function($scope, $http, $location) {
-  // if (loggedIn()){
-  //   $location.path('/dashboard');
-  // }
-  // else {
+
     $scope.login = function() {
       $http.get('/login').success(function(data) {
-          // $location.path('/dashboard');
           // window.location = "/#/dashboard"
       })
     }
-  // }
   }
 ])
