4b8efb0f8471de223584f66838c06561e4286f67
Andrea Braunstein <acbraunstein@gmail.com>
2014-12-03 12:14:13 -0800

    recipe_editor html moved to external file, no longer in dashboard.js

--- public/javascripts/controllers/dashboard.js
+++ public/javascripts/controllers/dashboard.js
@@ -19,13 +19,19 @@ app.controller("DashboardController", ["$scope", "$http", "$routeParams", "$loca
 
     }
 
-  $scope.clickToOpen = function (recipeImgUrl, recipeTitle, recipeSourceUrl, recipeCategory, recipeTagString, recipeId) {
-     var recipeTags = recipeTagString.split(", ");
+  $scope.addPinDialog = function(){
+    ngDialog.open({
+      template: 'templates/test_template.html',
+      scope: $scope
+    });
+  }
+
+  $scope.clickToOpen = function (recipe) {
+      $scope.recipe = recipe;
+
+     var recipeTags = recipe.tag_string.split(", ");
      $scope.recipeTags = recipeTags;
      $scope.myForm = {};
-     $scope.recipeCategory = recipeCategory;
-     $scope.recipeTitle = recipeTitle;
-     $scope.recipeId = recipeId;
      $scope.myForm.options = [
        { category_id : 1, name: "Appetizers" }
        ,{ category_id : 2, name: "Beverages" }
@@ -35,53 +41,8 @@ app.controller("DashboardController", ["$scope", "$http", "$routeParams", "$loca
        ,{ category_id : 6, name: "Salads" }
        ,{ category_id : 7, name: "Sides" }
       ];
-     console.log(recipeTagString);
       ngDialog.open({
-         template: '<div class="lightbox">' +
-                      '<div>' +
-                        '<table>' +
-                            '<tr>' +
-                              '<td>' +
-                                '<div class="custom_editing">' +
-                                  '<div class="lightbox_recipe_bg" style="background-image: url('+recipeImgUrl+')"></div>' +
-                                   '<div class= "edit_form_click">' +
-                                    '<div class="blur">' +
-                                    '</div>' +
-                                    '<div class="edit_form-text">' +
-                                      '<form class="recipe_edit_form">' +
-                                          '<label>Recipe Name</label>' +
-                                          '<input class="recipe_name_input" type="text" name="title" value="' + recipeTitle + '">' +
-                                          '<label>Category</label>' +
-                                          '<select ng-init="recipe.category_id = recipeCategory" ng-model="recipe.category_id" ng-options="recipe.category_id as recipe.name for recipe in myForm.options"' +
-                                          'class="categories thick-txt-bx">' +
-                                          '</select><br>' +
-                                          '<label>Tags</label>' +
-                                          '<input name="tag_string" value="' + recipeTagString +'">' +
-                                          // '<input ng-repeat="tag in recipeTags" type="text" name="tag_string" value="{{tag}}">' +
-                                          '</form>' +
-                                         '<button ng-click="saveRecipe()">Save</button>' +
-                                         '<button ng-click="deleteRecipe($index)" value="Delete">Delete</button>' +
-                                        '<button ng-click="cancelEdit()">Cancel</button>' +
-                                      // '</form>' +
-                                    '</div>' +
-                                  '</div>' +
-                                '</div>' +
-                              '</td>' +
-                            '</tr>' +
-                            '<tr>' +
-                              '<td class="lightbox_caption">' +
-                                '<div class="float_left">' +
-                                  '<a  class="lightbox_link_label" href=' + recipeSourceUrl + ' target="_blank"><h2 class="recipeTitle">' + recipeTitle + '</h2></a>' +
-                                '</div>' +
-                                '<div class="float_right" >' +
-                                  '<button class="edit_btn" ng-click="editPin(); showEdit=true">Edit</button>' +
-                                '</div>' +
-                              '</td>' +
-                            '</tr>' +
-                          '</table>' +
-                        '</div>' +
-                    '</div>',
-        plain: true,
+        template: 'templates/recipe_editor.html',
         scope: $scope
       });
     };
@@ -123,7 +84,7 @@ app.controller("DashboardController", ["$scope", "$http", "$routeParams", "$loca
       for(var i = 0; i < $scope.recipes.length; i++)
       {
         var recipeIndexToDelete = -1;
-        if ($scope.recipes[i].title === $scope.recipeTitle)
+        if ($scope.recipes[i].title === $scope.recipe.title)
         {
           recipeIndexToDelete = i;
           $scope.recipes.splice(recipeIndexToDelete, 1);
