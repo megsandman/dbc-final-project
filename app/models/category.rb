dc0d3f9effe47aae749b319137c4d79a4ea62824
cgdibble <cgdibble@gmail.com>
2014-12-04 15:04:05 -0800

    removed key value reference and moved it to controller

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   def self.get_category_id(category_name)
-    category = Category.find_by_name(category_name[:category])
+    category = Category.find_by_name(category_name)
     category.id
   end
 
